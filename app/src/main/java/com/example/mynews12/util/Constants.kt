f7c16350a531c9be3a2c047eae0e13ccdadee912
Bintina <105106238+Bintina@users.noreply.github.com>
2023-08-27 06:35:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynews12/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.mynews12.util
+
+object Constants {
+    // Top Stories API url
+    //https://api.nytimes.com/svc/topstories/v2/world.json?api-key=zISBuhDClXAT0G1Lpmz0YnbaGfhPkGqf
+
+    //New York Times Top Stories base Url
+    val BASE_URL = "https://api.nytimes.com/svc/topstories/v2/"
+
+    //End Url: /world.json?api-key=yourkey
+    const val END_URL = "world.json?api-key=zISBuhDClXAT0G1Lpmz0YnbaGfhPkGqf"
+
+}
